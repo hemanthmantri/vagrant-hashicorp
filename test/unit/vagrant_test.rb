ff677f839698a8f3c7b50b5c3896474c938bde7c
Hemanth Mantri <hemanth.mantri@harness.io>
2023-12-06 09:32:25 -0800

    Unset changes to test/unit/vagrant_test.rb

--- test/unit/vagrant_test.rb
+++ test/unit/vagrant_test.rb
@@ -2,9 +2,7 @@
 # SPDX-License-Identifier: BUSL-1.1
 
 require File.expand_path("../base", __FILE__)
-if ENV['TI'] == 1
-  require_relative '/tmp/engine/ruby/harness/ruby-agent/test_intelligence.rb'
-end
+
 describe Vagrant do
   include_context "unit"
 
